ad7f5014ab4d6cd61c5af3b0bd3de42bbc499c09
xtina-lt <xtina_lt@outlook.com>
2022-03-31 09:47:41 -0400

    test

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,73 +1,95 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, get_flashed_messages, session
 from flask_app import app
 from flask_app.models.user import User
+from flask_app.models.message import Message
+from flask_app.models.product import Product
+from flask_app.models.like import Like
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
-
-@app.route('/')
+'''HOME'''
+@app.route("/")
 def index():
-    return render_template("index.html")
+    likes=Like.select_all()
+    return render_template("index.html", likes = likes)
 
-'''READ'''
-# read all
-@app.route("/read")
-def read():
-    select_all = User.select_all()
-    # returns a list of classes
-    return render_template("output.html", output = select_all)
+'''LOGIN / REGISTER'''
+@app.route("/login")
+def login_reg():
+    return render_template("log_reg.html")
 
-@app.route("/read_one/<int:id>")
-def read_one(id):
-    data={'id':id}
-    result=User.get_one(data)
-    return render_template("output_one.html", output = result)
+@app.route("/login/process", methods=["POST"])
+def login_process():
+    data=request.form
+    if not User.validate_login(data):
+        return redirect("/login")
+    else:
+        session["logged_in"] = User.get_stars_email(data)
+        print(session)
+        return redirect("/dashboard")
 
-'''CREATE'''
-@app.route("/create")
-def new():
-    return render_template("create.html")
+@app.route("/register/process", methods=["POST"])
+def create_process():
+    data={k:v for k,v in request.form.items()}
+    if not User.validate_insert(data):
+        return redirect("/login")
+    else:
+        print(data)
+        data["password"] = bcrypt.generate_password_hash(request.form['password'])
+        User.insert(data)
+        session["logged_in"] = User.get_stars_email(data)
+        return redirect("/dashboard")
 
-@app.route('/create_new', methods=["POST"])
-def create():
-    data ={k:v for k,v in request.form.items()}
-    print(data)
-    # {'first_name': 'x', 'last_name': 'x', 'email': 'x', 'password': 'x'}
-    # creates a dictionary with the form names and values
-    new_user=User.insert(data)
-    # save the new user incase you want to use it later
-    return redirect('/read')
+'''LOGOUT'''
+@app.route("/logout")
+def logout_process():
+    session.clear()
+    return redirect("/")
 
+'''DASHBOARD'''
+@app.route("/dashboard")
+def dashboard():
+    if session["logged_in"]["id"] == 1:
+        u = session['logged_in']
+        recieved = Message.read_recieved(u)
+        sent = Message.read_sent(u)
+        users = User.select_all()
+        user = User.select_one(u)
+        likes = Like.select_all()
+        return render_template("dash.html", recieved=recieved, sent=sent, admin=True, users=users, user=user, likes=likes)
+    else:
+        u = session['logged_in']
+        recieved = Message.read_recieved(u)
+        sent = Message.read_sent(u)
+        user = User.select_one(u)
+        products = Product.check_product_user(u)
+        user_likes = Like.select_by_user(u)
+        likes = Like.select_all()
+        return render_template("dash.html", recieved=recieved, sent=sent, user=user, products=products, user_likes=user_likes, likes=likes)
 
 '''UPDATE'''
-@app.route('/edit/<int:id>')
-def show(id):
-    data ={ 
-    # get data from url variable
-        "id":id
-        #insert as id
-    }
-    output=User.get_one(data)
-    # use the get_one fucntion in the User class
-    # save to pass to html page
-    return render_template("edit.html", output=output)
+@app.route("/user/update")
+def edit_show():
+    data={"id" : session['logged_in']['id']}
+    result = User.select_one(data)
+    return render_template("edit.html", output=result)
 
-@app.route('/process_edit',methods=["POST"])
-def process_edit():
+@app.route("/user/update/process", methods=["POST"])
+def edit_process():
     data={k:v for k,v in request.form.items()}
-    User.update(data)
-    # from the user class, use the update method, 
-    # pass in arg of data from request.form
-    return redirect('/read')
+    if not User.validate_update(data):
+        return redirect(f"/user/update/{data['id']}") 
+    else:
+        if data["new_pass"]:
+            data["password"] = bcrypt.generate_password_hash(data["new_pass"])
+            User.update(data)
+        else:
+            data["password"] = bcrypt.generate_password_hash(data["password"])
+            User.update(data)
+        return redirect(f"/user/read/{data['id']}")
 
-
-'''DELETE'''
-@app.route("/delete/<int:id>")
-def delete(id):
-    data={'id':id}
-    print(data)
-    User.delete(data)
-    return redirect('/read')
-
-@app.route('/', defaults={"path":""})
-@app.route('/<path:path>')
+'''CATCHALL'''
+@app.route("/", defaults={"path":""})
+@app.route("/<path:path>")
 def catch_all(path):
     return render_template("catchall.html")
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask_app import app
 # 1) import flask application initiated in __init__.py
 
-from flask_app.controllers import users
+from flask_app.controllers import users, replies, projects, products, messages, likes
 # 2) import routes
 
 if __name__ == "__main__":
